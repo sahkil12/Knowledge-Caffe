6d8b4adde08ad4adaa1fd2667c635b676a9745d6
Tazwer Shakil <tazwershakilshakil@gmail.com>
2025-08-26 18:22:36 +0600

    add tailwind and react and some get some api data

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Blogs from './assets/Components/Blogs.jsc/Blogs'
+import Header from './assets/Components/Header/Header'
+
+function App() {
+
+  return (
+    <>   
+     <Header></Header>
+     <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
